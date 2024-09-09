6c60af657c887c77e2902b17e52b4d096e44f2ad
leshiivoy <leshiivoy@gmail.com>
2024-09-09 11:15:22 +0200

    mod 13.3

---
+++ module_13_3.py
@@ -0,0 +1,24 @@
+import asyncio
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=["start"])
+async def start_message(message):
+    print("Start message")
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+
+@dp.message_handler()
+async def all_message(message):
+    print("Мы получили сообщение!")
+    await message.answer('Введите команду /start что бы начать общение')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
